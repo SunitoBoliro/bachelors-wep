42b03a14b1132d9b213635dae0baf327c82980ce
AHSAN AHMED KHAN <115715056+SunitoBoliro@users.noreply.github.com>
2023-06-18 14:54:24 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+
+import { logo } from './assets';
+import { Home, CreatePost } from './page';
+
+const App = () => (
+  <BrowserRouter>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#38CCCCFF] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </main>
+  </BrowserRouter>
+);
+
+export default App;
